1dbdebc97d8bd512590c0ba9f188cb1e56cffdd2
adrianohrl <me@adrianohrl.tech>
2018-05-17 14:34:44 -0300

    created login form

--- src/main/java/tech/adrianohrl/identistry/iDentistry.java
+++ src/main/java/tech/adrianohrl/identistry/iDentistry.java
@@ -7,7 +7,13 @@ package tech.adrianohrl.identistry;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Locale;
 import javax.swing.*;
+import javax.swing.plaf.metal.DefaultMetalTheme;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.OceanTheme;
+import jiconfont.icons.FontAwesome;
+import jiconfont.swing.IconFontSwing;
 import tech.adrianohrl.identistry.view.LoginDialog;
 
 /**
@@ -15,13 +21,65 @@ import tech.adrianohrl.identistry.view.LoginDialog;
  * @author adrianohrl
  */
 public class iDentistry {
+    
+    private final static String LOOKANDFEEL = "System";
+    private final static String THEME = "Ocean";
+    
+    private static void setLookAndFeel() {
+        String lookAndFeel = null;
+       
+        if (LOOKANDFEEL != null) {
+            if (LOOKANDFEEL.equals("Metal")) {
+                lookAndFeel = UIManager.getCrossPlatformLookAndFeelClassName();
+              //  an alternative way to set the Metal L&F is to replace the 
+              // previous line with:
+              // lookAndFeel = "javax.swing.plaf.metal.MetalLookAndFeel";
+                
+            } else if (LOOKANDFEEL.equals("System")) {
+                lookAndFeel = UIManager.getSystemLookAndFeelClassName();
+            } else if (LOOKANDFEEL.equals("Motif")) {
+                lookAndFeel = "com.sun.java.swing.plaf.motif.MotifLookAndFeel";
+            } else if (LOOKANDFEEL.equals("GTK")) { 
+                lookAndFeel = "com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
+            } else {
+                System.err.println("Unexpected value of LOOKANDFEEL specified: " + LOOKANDFEEL);
+                lookAndFeel = UIManager.getCrossPlatformLookAndFeelClassName();
+            }
+            try {
+                UIManager.setLookAndFeel(lookAndFeel);
+                if (LOOKANDFEEL.equals("Metal")) {
+                  if (THEME.equals("DefaultMetal")) {
+                      MetalLookAndFeel.setCurrentTheme(new DefaultMetalTheme());
+                  } else if (THEME.equals("Ocean")) {
+                     MetalLookAndFeel.setCurrentTheme(new OceanTheme());
+                  }
+                  UIManager.setLookAndFeel(new MetalLookAndFeel()); 
+                }	                
+            } catch (ClassNotFoundException e) {
+                System.err.println("Couldn't find class for specified look and feel:" + lookAndFeel);
+                System.err.println("Did you include the L&F library in the class path?");
+                System.err.println("Using the default look and feel.");
+            } catch (UnsupportedLookAndFeelException e) {
+                System.err.println("Can't use the specified look and feel (" + lookAndFeel + ") on this platform.");
+                System.err.println("Using the default look and feel.");
+            } catch (InstantiationException | IllegalAccessException e) {
+                System.err.println("Couldn't get specified look and feel (" + lookAndFeel + "), for some reason.");
+                System.err.println("Using the default look and feel.");
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void main(String[] args) {
+        IconFontSwing.register(FontAwesome.getIconFont());
+        Locale.setDefault(new Locale("pt", "BR"));
+        setLookAndFeel();
         final JFrame frame = new JFrame("JDialog Demo");
-        final JButton btnLogin = new JButton("Click to login");
- 
+        Icon icon = IconFontSwing.buildIcon(FontAwesome.SMILE_O, 40, new Color(0, 150, 0));
+        final JButton btnLogin = new JButton("Click to login", icon);
         btnLogin.addActionListener(
                 new ActionListener(){
+                    @Override
                     public void actionPerformed(ActionEvent e) {
                         LoginDialog loginDlg = new LoginDialog(frame);
                         loginDlg.setVisible(true);
